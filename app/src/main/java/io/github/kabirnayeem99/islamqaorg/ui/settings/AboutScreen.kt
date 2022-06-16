a554acf8987b6e39c58bc1912cb214519c7ca3a8
Aaron M. Wilson <39023212+kabirnayeem99@users.noreply.github.com>
2022-06-16 18:09:41 +0600

    🗜️ refactor [settings]: refactor about screen in jetpack compose

--- app/src/main/java/io/github/kabirnayeem99/islamqaorg/ui/settings/AboutScreen.kt
+++ app/src/main/java/io/github/kabirnayeem99/islamqaorg/ui/settings/AboutScreen.kt
@@ -1,15 +1,21 @@
 package io.github.kabirnayeem99.islamqaorg.ui.settings
 
-import androidx.compose.animation.*
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.*
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.Build
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -20,7 +26,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.platform.UriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
@@ -52,9 +61,71 @@ fun AboutScreen(navigator: DestinationsNavigator) {
                 Spacer(modifier = Modifier.height(62.dp))
                 val aboutLabel = stringResource(id = R.string.label_about)
                 ScreenTitle(homePageTitle = aboutLabel)
+            }
+            item {
                 AboutContent()
             }
+            item {
+                Spacer(modifier = Modifier.height(15.dp))
+                Text(
+                    text = stringResource(id = R.string.about_app),
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        fontStyle = FontStyle.Italic,
+                    )
+                )
+            }
+            item {
+                Spacer(modifier = Modifier.height(15.dp))
+                AboutButtonSection()
+                Spacer(modifier = Modifier.height(15.dp))
+            }
         }
+    }
+}
+
+@Composable
+private fun AboutButtonSection() {
+
+    val uriHandler: UriHandler = LocalUriHandler.current
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        AboutBoxItem(
+            icon = Icons.Outlined.Info,
+            title = stringResource(id = R.string.label_about)
+        ) {
+            navigateToIslamQaAboutSection(uriHandler)
+        }
+        Spacer(modifier = Modifier.width(12.dp))
+        AboutBoxItem(
+            icon = Icons.Outlined.Build,
+            title = stringResource(id = R.string.content_desc_open_github_source_code)
+        ) {
+            navigateToSourceCode(uriHandler)
+        }
+        Spacer(modifier = Modifier.width(12.dp))
+        AboutBoxItem(
+            icon = Icons.Outlined.Person,
+            title = stringResource(id = R.string.content_desc_about_developer)
+        ) {
+            navigateToMyPersonalGitHubProfile(uriHandler)
+        }
+    }
+}
+
+@Composable
+private fun AboutBoxItem(icon: ImageVector, title: String, onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .width(60.dp)
+            .height(60.dp)
+            .clip(CircleShape)
+            .background(MaterialTheme.colorScheme.onPrimaryContainer)
+            .clickable { onClick() }, contentAlignment = Alignment.Center
+    ) {
+        Image(imageVector = icon, contentDescription = title)
     }
 }
 
@@ -78,34 +149,25 @@ private fun AboutTopAppBar(navigator: DestinationsNavigator) {
 }
 
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 private fun AboutContent() {
     val scope = rememberCoroutineScope()
 
-    var centerAppLogoVisibility by remember { mutableStateOf(false) }
     var appVersionVisibility by remember { mutableStateOf(false) }
-    var geometryVisibility by remember { mutableStateOf(false) }
 
     val infiniteTransition = rememberInfiniteTransition()
     val angle by infiniteTransition.animateFloat(
         initialValue = 0F,
         targetValue = 360F,
         animationSpec = infiniteRepeatable(
-            animation = tween(SPLASH_SCREEN_DURATION.toInt(), easing = FastOutSlowInEasing)
+            animation = tween((SPLASH_SCREEN_DURATION * 2).toInt(), easing = LinearEasing)
         )
     )
 
     LaunchedEffect(true) {
         scope.launch {
             delay(SPLASH_SCREEN_DURATION / 6)
-            centerAppLogoVisibility = true
-            delay(SPLASH_SCREEN_DURATION / 6)
-            geometryVisibility = true
-            delay(SPLASH_SCREEN_DURATION / 6)
             appVersionVisibility = true
-            delay(SPLASH_SCREEN_DURATION / 3)
-            delay(SPLASH_SCREEN_DURATION / 3)
         }
     }
 
@@ -117,54 +179,41 @@ private fun AboutContent() {
     ) {
 
 
-        AnimatedVisibility(
-            visible = geometryVisibility,
-            enter = scaleIn() + fadeIn(),
-            exit = scaleOut() + fadeOut(),
-        ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_islamic_geometric),
+            contentDescription = stringResource(id = R.string.content_desc_app_logo),
+            contentScale = ContentScale.Fit,
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onPrimaryContainer),
+            alignment = Alignment.Center,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .height(350.dp)
+                .width(350.dp)
+                .graphicsLayer {
+                    rotationZ = angle
+                }
+        )
 
+
+        Box(
+            modifier = Modifier
+                .width(260.dp)
+                .height(260.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.onPrimaryContainer)
+                .align(Alignment.Center),
+            contentAlignment = Alignment.Center,
+        ) {
             Image(
-                painter = painterResource(id = R.drawable.ic_islamic_geometric),
+                painter = painterResource(id = R.drawable.ic_islamqa),
                 contentDescription = stringResource(id = R.string.content_desc_app_logo),
                 contentScale = ContentScale.Fit,
-                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onPrimaryContainer),
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primaryContainer),
                 alignment = Alignment.Center,
                 modifier = Modifier
                     .align(Alignment.Center)
-                    .height(350.dp)
-                    .width(350.dp)
-                    .graphicsLayer {
-                        rotationZ = angle
-                    }
+                    .padding(top = 22.dp)
             )
-
-        }
-
-        AnimatedVisibility(
-            visible = centerAppLogoVisibility,
-            enter = scaleIn() + fadeIn(),
-            exit = scaleOut() + fadeOut(),
-        ) {
-            Box(
-                modifier = Modifier
-                    .width(260.dp)
-                    .height(260.dp)
-                    .clip(CircleShape)
-                    .background(MaterialTheme.colorScheme.onPrimaryContainer)
-                    .align(Alignment.Center),
-                contentAlignment = Alignment.Center,
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_islamqa),
-                    contentDescription = stringResource(id = R.string.content_desc_app_logo),
-                    contentScale = ContentScale.Fit,
-                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primaryContainer),
-                    alignment = Alignment.Center,
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                        .padding(top = 22.dp)
-                )
-            }
         }
 
         AnimatedVisibility(
@@ -192,4 +241,23 @@ private fun AboutContent() {
             }
         }
     }
+}
+
+
+private const val GITHUB_SOURCE_CODE_LINK = "https://github.com/kabirnayeem99/islam_qa_org_android"
+
+private fun navigateToSourceCode(uriHandler: UriHandler) {
+    uriHandler.openUri(GITHUB_SOURCE_CODE_LINK)
+}
+
+private const val ISLAMQA_ABOUT_SECTION_LINK = "https://islamqa.org/about/"
+
+private fun navigateToIslamQaAboutSection(uriHandler: UriHandler) {
+    uriHandler.openUri(ISLAMQA_ABOUT_SECTION_LINK)
+}
+
+private const val PERSONAL_GITHUB_PROFILE_LINK = "https://github.com/kabirnayeem99/"
+
+private fun navigateToMyPersonalGitHubProfile(uriHandler: UriHandler) {
+    uriHandler.openUri(PERSONAL_GITHUB_PROFILE_LINK)
 }
