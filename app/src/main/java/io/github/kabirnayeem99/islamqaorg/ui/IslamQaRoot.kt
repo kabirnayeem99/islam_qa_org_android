41f548c3cb59dc741df468eaa047d7b9953f97bf
Naimul Kabir <kabirnayeem.99@gmail.com>
2024-02-13 13:39:26 +0600

    chore: remove deprecated codes

--- app/src/main/java/io/github/kabirnayeem99/islamqaorg/ui/IslamQaRoot.kt
+++ app/src/main/java/io/github/kabirnayeem99/islamqaorg/ui/IslamQaRoot.kt
@@ -1,11 +1,7 @@
 package io.github.kabirnayeem99.islamqaorg.ui
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.rememberNavHostEngine
 import io.github.kabirnayeem99.islamqaorg.ui.theme.IslamQaTheme
@@ -13,13 +9,6 @@ import io.github.kabirnayeem99.islamqaorg.ui.theme.IslamQaTheme
 @Composable
 fun IslamQaRoot() {
     IslamQaTheme {
-
-        val systemUiController = rememberSystemUiController()
-        val shouldUseDarkIcon = !isSystemInDarkTheme()
-
-        SideEffect {
-            systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = shouldUseDarkIcon)
-        }
 
         val navController = rememberNavController()
         val navHostEngine = rememberNavHostEngine()
